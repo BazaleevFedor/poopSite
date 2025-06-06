0b930b2366368ffaeb0c880bfdd16ea51a36c5b4
bazaleev.fedor <bazaleev.fedor@wb.ru>
2025-06-06 19:57:01 +0300

    add ci.yml and example test

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'public/**/*.{ts,tsx}',
+    '!public/**/*.d.ts'
+  ],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+};
---
+++ public/__tests__/app.test.ts
@@ -0,0 +1,7 @@
+import { Card } from '../src/components/card/card';
+
+describe('Example test', () => {
+    it('should pass', () => {
+        expect(true).toBe(true);
+    });
+});
